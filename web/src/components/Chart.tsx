e70bc11f4cfd41d40e0206812dedc598a914bc0a
Antony90 <antony.antoniou18@gmail.com>
2022-11-03 22:45:27 +0000

    fixed bug: chart background color not updating

--- web/src/components/Chart.tsx
+++ web/src/components/Chart.tsx
@@ -1,19 +1,22 @@
 import React, { useEffect, useRef, useState } from "react";
 import ChartsEmbedSDK from "@mongodb-js/charts-embed-dom";
 import { Card, useMantineColorScheme } from "@mantine/core";
+import { useForceUpdate } from "@mantine/hooks";
 
-const sdk = new ChartsEmbedSDK({
-  baseUrl: "https://charts.mongodb.com/charts-project-0-mlqlp",
-});
 
 interface MongoDashboardProps {
   height: number;
   width: number;
   chartId: string;
 }
+const sdk = new ChartsEmbedSDK({
+  baseUrl: "https://charts.mongodb.com/charts-project-0-mlqlp",
+});
 
 const Chart: React.FC<MongoDashboardProps> = ({ chartId, height, width }) => {
+  const [rendered, setRendered] = useState(false);
   const { colorScheme } = useMantineColorScheme();
+
   const [chart] = useState(
     sdk.createChart({
       chartId,
@@ -22,22 +25,26 @@ const Chart: React.FC<MongoDashboardProps> = ({ chartId, height, width }) => {
       theme: colorScheme,
       background: "transparent"
     })
-  );
-
+  )
   const chartDiv = useRef<HTMLSpanElement>(null);
 
   useEffect(() => {
     if (chartDiv.current !== null) {
       chart.render(chartDiv.current)
+      .then(() => setRendered(true));
+      // HACK: fixes MongoDB chart putting
+      // white background on white text in dark mode
+      chartDiv.current.style.colorScheme = "auto";
     }
   }, [chart]);
-
+  
   useEffect(() => {
-    chart.setTheme(colorScheme);
+    if (rendered)
+      chart.setTheme(colorScheme);
   }, [colorScheme])
 
-  return (<Card shadow="lg" radius="lg" p="lg" m="sm" withBorder>
-      <span ref={chartDiv} className="chart"/>;
+  return (<Card shadow="lg" radius="lg" p="xs" m="sm" withBorder>
+      <span ref={chartDiv} className="chart"/>
     </Card>)
 };
 
--- web/src/components/pages/AnalyticsPage.tsx
+++ web/src/components/pages/AnalyticsPage.tsx
@@ -1,14 +1,35 @@
-import { Group, Stack } from "@mantine/core";
-import React from "react";
+import { Card, Grid, Group, ThemeIcon, Title } from "@mantine/core";
+import { IconNews } from "@tabler/icons";
 import Chart from "../Chart";
 
 const AnalyticsPage = () => {
   return (
-    <Group position="center">
-      <Chart height={450} width={600} chartId="6362adf6-680d-428e-8fc5-9be133f827d0" />
-      <Chart height={450} width={600} chartId="635de3ab-8f1f-45e3-8428-05b934b9895b" />
-    </Group>
-  );
+    <Grid>
+      <Chart height={420} width={500} chartId="6362adf6-680d-428e-8fc5-9be133f827d0" />
+      <Chart height={420} width={500} chartId="635de3ab-8f1f-45e3-8428-05b934b9895b" />
+      <Chart height={420} width={500} chartId="636303ce-fb36-4a0f-80f7-8ea226f58558" />
+      <Card shadow="lg" radius="lg" p="xl" m="sm" withBorder>
+        <Group>
+          <ThemeIcon variant="default" size="xl">
+            <IconNews size={30} />
+          </ThemeIcon>
+          <Title order={2}>
+            {(459024).toLocaleString()} articles analyzed.
+          </Title>
+        </Group>
+      </Card>
+      <Card shadow="lg" radius="lg" p="xl" m="sm" withBorder>
+        <Group>
+          <ThemeIcon variant="default" size="xl">
+            <IconNews size={30} />
+          </ThemeIcon>
+          <Title order={3}>
+            {-0.643} average sentiment score
+          </Title>
+        </Group>
+      </Card>
+    </Grid>
+  )
 };
 
 export default AnalyticsPage;
